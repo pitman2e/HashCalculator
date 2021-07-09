dde3fe81e578def5bcfd0e11868a1309f55b6cb8
pitman2e <pitman2e@gmail.com>
2021-07-09 22:33:25 +0800

    Scan interval is default to 365

--- HashCalculator/CmdOptions.cs
+++ HashCalculator/CmdOptions.cs
@@ -8,7 +8,7 @@ namespace HashCalculator
         [Option('d', "directory", Required = true, HelpText = "Root directory to scan")]
         public string Directory { get; set; }
         
-        [Option('i', "interval", Required = true, HelpText = "Days before rescan")]
+        [Option('i', "interval", Default = 365, Required = false, HelpText = "Days before rescan")]
         public int ScanInterval { get; set; }
 
         [Option('v', "verbose", Required = false, HelpText = "Print verbose message")]
